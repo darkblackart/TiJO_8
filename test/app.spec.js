d47170b12f7497e3011415bb909cf27cba8c7e23
Jacek Żebrowski <zebrowski27200@student.pwsztar.edu.pl>
2017-05-29 14:31:56 +0200

    Function 2

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,6 +8,17 @@ describe('reverseNumber', function () {
         expect(app.reverseNumber(1337)).to.eql(7331);
     });
 
+    it('should return false', function () {
+        expect(app.reverseNumber('cos')).to.eql(false);
+    })
+
+    it('should return false', function () {
+        expect(app.reverseNumber('@!$')).to.eql(false);
+    })
+
+    it('should return 0x3D', function () {
+        expect(app.reverseNumber(0x10)).to.eql(0x3D);
+    })
 });
 
 describe('returnOnlyLetter', function () {
@@ -15,11 +26,29 @@ describe('returnOnlyLetter', function () {
         expect(app.returnOnlyLetter('cos')).to.eql('cos');
     });
 
+    it('should return \'lt\'', function () {
+        expect(app.returnOnlyLetter('l33t./$%@™')).to.eql('lt');
+    })
+
+    it('should return false', function () {
+        expect(app.returnOnlyLetter(0x0E)).to.eql(false);
+    })
+
+    it('should return \'\'', function () {
+        expect(app.returnOnlyLetter('')).to.eql('');
+    })
+
+    it('should return \'\'', function () {
+        expect(app.returnOnlyLetter('')).to.eql('');
+    })
 });
 
 describe('isEmail', function () {
     it('should return true', function () {
-        expect(app.isEmail('23b234234@wp.pl')).to.eql(true);
+        expect(app.isEmail('habababababa@wp.pl')).to.eql(true);
     });
 
+    it('should return true', function () {
+        expect(app.isEmail('habababababa.habababababa')).to.eql(false);
+    });
 });
