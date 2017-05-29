785e4f9aef06de0704d20205f4867b25b86ed464
Jacek Żebrowski <zebrowski27200@student.pwsztar.edu.pl>
2017-05-29 14:29:05 +0200

    Function 1

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,17 +3,23 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
-describe('reverseNumber', function ()
-{
+describe('reverseNumber', function () {
+    it('should reverse number', function () {
+        expect(app.reverseNumber(1337)).to.eql(7331);
+    });
 
 });
 
-describe('returnOnlyLetter', function ()
-{
+describe('returnOnlyLetter', function () {
+    it('should return \'cos\'', function () {
+        expect(app.returnOnlyLetter('cos')).to.eql('cos');
+    });
 
 });
 
-describe('isEmail', function ()
-{
+describe('isEmail', function () {
+    it('should return true', function () {
+        expect(app.isEmail('23b234234@wp.pl')).to.eql(true);
+    });
 
 });
